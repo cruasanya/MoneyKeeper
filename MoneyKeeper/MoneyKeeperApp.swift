d5554c40d97d0dac00acfcc3ce26f4a24553dc67
cruasanya <98823717+cruasanya@users.noreply.github.com>
2023-11-13 13:49:41 +0200

    Initial Commit

---
+++ MoneyKeeper/MoneyKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyKeeperApp.swift
+//  MoneyKeeper
+//
+//  Created by Александр Новиков on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoneyKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
